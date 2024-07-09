757eb2b50cd00eb6151e4993da292042b32dae27
Isapov Aziz <a.isapov@globerce.capital>
2024-07-09 15:58:51 +0500

    Удалил не используемые импорты

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Express, NextFunction, Request, Response} from "express";
+import express, {Express} from "express";
 import { Server } from "http";
 import {LoggerService} from "./logger/logger.service";
 import {UserController} from "./users/user.controller";
--- src/common/types.ts
+++ src/common/types.ts
@@ -1,7 +1,3 @@
-import {ILogger} from "../logger/logger.service.interface";
-import {LoggerService} from "../logger/logger.service";
-import {UserController} from "../users/user.controller";
-
 const TYPES = {
     LoggerService: Symbol.for("LoggerService"),
     UserController: Symbol.for("UserController"),
